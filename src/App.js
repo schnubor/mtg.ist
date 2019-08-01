58d7645c046ed2c9ad4d6a189a143ac62013fb88
Christian Korndoerfer <schnuppser@gmail.com>
2019-08-01 18:19:36 +0200

    scss modules working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 // UI
-import styles from './App.scss';
+import styles from './App.module.scss';
 // Pages
 import RandomCardPage from './modules/randomCard/components/page'
 
