f73a9c21d65cc95c8349a22e3cdbbb384d2d945a
Christian Korndoerfer <schnuppser@gmail.com>
2019-08-05 23:05:50 +0200

    login screen

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import styles from './App.module.scss';
 // Pages
 import RandomCardPage from './modules/randomCard/components/page'
+import LoginPage from './modules/auth/components/LoginPage'
 
 function App () {
     return (
@@ -11,6 +12,7 @@ function App () {
             <Router>
                 <Switch>
                     <Route path="/" exact component={RandomCardPage}/>
+                    <Route path="/login" exact component={LoginPage}/>
                 </Switch>
             </Router>
         </div>
